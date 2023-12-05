9b45e80fd169a793cd34154bba543c2caf8c93b1
ALcitya <ichwanalif55@gmail.com>
2023-12-05 14:27:41 +0700

    merubah praktik 2 4 dan 6

--- praktikum_webpro3_js/praktik4.js
+++ praktikum_webpro3_js/praktik4.js
@@ -1,10 +1,11 @@
-function jumlah(){
+function jumlah()
+{
     var bil1 = parseFloat(document.fform.bilangan1.value)
-    if (isNAN (bil1));
+    if(isNaN (bil1))
     bil1 = 0.0;
     var bil2 = parseFloat(document.fform.bilangan2.value)
-    if (isNAN (bil2));
+    if(isNaN (bil2))
     bil2 = 0.0;
     var hasil = bil1 + bil2;
-    alert("Hasil penjumlahan = "+hasil);
+    alert("Hasil Penjumlahan = " + hasil);
 }
